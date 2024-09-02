b93b9c62093bc1ed111e186e450008978098a45c
Vladimir Gromov <v.dece1verr@hotmail.com>
2024-09-02 20:58:35 +0300

    -

---
+++ Vovannum/MachineInfo.cs
@@ -0,0 +1,62 @@
+﻿namespace Vovannum
+{
+    public class MachineInfo
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string FirstAxisName { get; set; }
+        public string SecondAxisName { get; set; }
+        public string ThirdAxisName { get; set; }
+        public string FourthAxisName { get; set; }
+        public string FifthAxisName { get; set; }
+        public string SixthAxisName { get; set; }
+        public string SeventhAxisName { get; set; }
+        public string EighthAxisName { get; set; }
+
+        public double FirstAxisAbsolutePosition { get; set; }
+        public double FirstAxisRelativePosition { get; set; }
+        public double FirstAxisMachinePosition { get; set; }
+        public double FirstAxisDistanceToGo { get; set; }
+
+        public double SecondAxisAbsolutePosition { get; set; }
+        public double SecondAxisRelativePosition { get; set; }
+        public double SecondAxisMachinePosition { get; set; }
+        public double SecondAxisDistanceToGo { get; set; }
+
+        public double ThirdAxisAbsolutePosition { get; set; }
+        public double ThirdAxisRelativePosition { get; set; }
+        public double ThirdAxisMachinePosition { get; set; }
+        public double ThirdAxisDistanceToGo { get; set; }
+
+        public double FourthAxisAbsolutePosition { get; set; }
+        public double FourthAxisRelativePosition { get; set; }
+        public double FourthAxisMachinePosition { get; set; }
+        public double FourthAxisDistanceToGo { get; set; }
+
+        public double FifthAxisAbsolutePosition { get; set; }
+        public double FifthAxisRelativePosition { get; set; }
+        public double FifthAxisMachinePosition { get; set; }
+        public double FifthAxisDistanceToGo { get; set; }
+
+        public double SixthAxisAbsolutePosition { get; set; }
+        public double SixthAxisRelativePosition { get; set; }
+        public double SixthAxisMachinePosition { get; set; }
+        public double SixthAxisDistanceToGo { get; set; }
+
+        public double SeventhAxisAbsolutePosition { get; set; }
+        public double SeventhAxisRelativePosition { get; set; }
+        public double SeventhAxisMachinePosition { get; set; }
+        public double SeventhAxisDistanceToGo { get; set; }
+
+        public double EighthAxisAbsolutePosition { get; set; }
+        public double EighthAxisRelativePosition { get; set; }
+        public double EighthAxisMachinePosition { get; set; }
+        public double EighthAxisDistanceToGo { get; set; }
+
+        public string Status { get; set; }
+        public string Mode { get; set; }
+        public int Speed { get; set; }
+        public int Feed { get; set; }
+    }
+}
---
+++ Vovannum/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Vovannum
+{
+    public static class Utils
+    {
+        public static void LogError(Exception ex)
+        {
+            ex.Log();
+        }
+    }
+}
