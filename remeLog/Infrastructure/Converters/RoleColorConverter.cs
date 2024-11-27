0dec27eceecaab2916c5ac75fbe92e2ff2b95f2b
Vladimir Gromov <v.dece1verr@hotmail.com>
2024-11-28 00:07:12 +0300

    цвета

---
+++ remeLog/Infrastructure/Converters/RoleColorConverter.cs
@@ -0,0 +1,34 @@
+﻿using remeLog.Infrastructure.Types;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace remeLog.Infrastructure.Converters
+{
+    public class RoleColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is User currentRole && parameter is string roleParameter)
+        {
+            if (Enum.TryParse(roleParameter, out User comparedRole))
+            {
+                return currentRole == comparedRole 
+                    ? Brushes.Black 
+                    : Brushes.Gray;
+            }
+        }
+        return Brushes.Gray;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+}
